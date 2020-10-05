723503de5e0200f2fac9a4c9971da8e9ea961aaa
chi_thanh_allgrow-labo <chi_thanh@allgrow-labo.jp>
2020-10-05 10:51:08 +0700

    Add midleware on file server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get(/.*/, function (req, res) {
 	res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
+app.use(express.static(path.join(__dirname, "client", "dist")))
+
+
 const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`app is listening on port: ${port}`)
